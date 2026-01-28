5a41fa9c2bdb4707016cc339870c1298a3bb8f52
IanBakke <ibakke@ucsc.edu>
2026-01-27 23:57:10 -0800

    Add files via upload

--- journal2.py
+++ journal2.py
@@ -2,7 +2,11 @@ x = 8.4
 print("x =", x)
 y = 3.3
 print("y =", y)
+a = 9
+print("a =", a)
+b = 4
+print("b =", b)
 
 print("x + y =", x + y)
-print("x - y =", x - y)
-print("x*y =", x * y)
+print("a - b =", a - b)
+print("x*a =", x * a)
